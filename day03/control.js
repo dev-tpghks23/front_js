c4ba6fc673758480b23cd3b4c536bd2c8de097f1
dev-tpghks23 <tpghksgit23@gmail.com>
2024-10-04 21:36:44 +0900

    js 수업

---
+++ day03/control.js
@@ -0,0 +1,46 @@
+// 성인인지 아닌지 검사하기
+// const age = 15;
+// const baseAge = 19;
+
+// if(조건식){}
+// if(age > baseAge){
+//   console.log("성인입니다.")
+// }else if(age == baseAge){
+//   console.log("내년에 만나자")
+// }else{
+//   console.log("성인이 아닙니다.😎")
+// }
+
+// switch문
+switch(3){
+  case 1:
+    console.log("1번");
+    break;
+  case 2:
+    console.log("2번");
+    break;
+  case 3:
+    console.log("3번");
+    break;
+  case 4:
+    console.log("4번");
+    break;
+  case 5:
+    console.log("5번");
+    break;
+  default:
+    console.log("1번 아님");
+    break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
